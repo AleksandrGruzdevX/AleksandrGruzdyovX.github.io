f43ff4a0d6ff12e39d36d986cce5b36f81e4190c
AleksandrGruzdevX <155438711+AleksandrGruzdevX@users.noreply.github.com>
2025-09-30 12:03:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function check(){
+    let x = document.getElementById("NameT");
+    let vk = document.getElementById("VK");
+    let vkval = vk.value;
+    let svk = "https://vk.com"
+    let y = x.value;
+    if (y == ""){
+        alert("Поле имя не заполнена");
+    }
+    if (vkval.includes(svk) == true){
+        return true;
+    }else{alert("Не указан VK")}
+}
